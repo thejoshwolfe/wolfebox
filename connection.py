1a25a2a43262e8d213e6ca24bd7ecf22e4420a0f
Josh Wolfe <thejoshwolfe@gmail.com>
2010-12-14 10:13:39 -0700

    moving Connection class to connection.py

--- connection.py
+++ connection.py
@@ -2,6 +2,8 @@
 class Connection:
     def __init__(self, actual_connection):
         self.connection = actual_connection
+    def __del__(self):
+        self.connection.close()
     def read(self, length):
         chunks = []
         while length != 0:
@@ -10,7 +12,7 @@ class Connection:
                 break
             chunks.append(chunk)
             length -= len(chunk)
-        return "".join(chunks)
+        return b"".join(chunks)
     def read_fmt(self, fmt):
         data = self.read(struct.calcsize(fmt))
         return struct.unpack(fmt, data)[0]
@@ -20,7 +22,7 @@ class Connection:
         return self.read_fmt("Q")
     def read_string(self):
         length = self.read_int()
-        return self.read(length)
+        return str(self.read(length), "utf8")
 
     def write(self, data):
         self.connection.sendall(data)
@@ -32,8 +34,20 @@ class Connection:
     def write_long(self, value):
         self.write_fmt("Q", value)
     def write_string(self, value):
+        value = bytes(value, "utf8")
         self.write_int(len(value))
         self.write(value)
 
     def ok(self):
-        self.write("w") # 'w' for "wolfebin"
+        """sent by the server after all the parameters have been received and validated"""
+        self.write(b"w") # 'w' for "wolfebin"
+    def check(self):
+        """read by the client"""
+        status_code = self.read(1)
+        if status_code == b"w":
+            return None
+        elif status_code == b"b":
+            return self.read_string()
+        else:
+            return "fatal: corrupt network communication " + repr(status_code + self.read(0x10))
+
